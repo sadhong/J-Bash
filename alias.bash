c88a47d632f8d512c2c91473e790f6fa79f7b8d7
sadhong <sadhong@gmail.com>
2012-12-01 00:52:56 +0800

    2012.11.30
    Jason's iMac Configration

---
+++ alias.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+alias ls='ls -FG'
+alias ll='ls -alh'
+alias l='ls -ah'
+alias c='clear'
+alias py='python'
+alias md='mkdir -p'
+alias rd='rmdir'
---
+++ prompt.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+PS1="(\t) ☾★ [\[$blue\]\u\[$reset_color\]@\[$green\]\H\[$reset_color\]] \[$yellow\]\w\[$reset_color\] ---> "
+PS2='> '
+PS4='+ '
